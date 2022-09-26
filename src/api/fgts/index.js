3c16c62a59dddd170bb66074d7e18753b1cbcb1a
unknown <izaiascrs@gmail.com>
2022-09-26 10:33:02 -0300

    add new routes

--- src/api/fgts/index.js
+++ src/api/fgts/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const { getToken } = require('../../functions/fgts'); 
 const axios = require('axios');
+const FormData = require('form-data');
+const {
+    getToken, 
+    sendWhatsAppLink,
+    fgtsSendFluxo,
+    fgtsSendWhatsappMessage,
+    whatsappCreateUser,
+    whatsappGetUserIdByPhone 
+} = require('../../functions/fgts'); 
 
 const router = express.Router();
 
 const apiCredentials = {}
+const cache = {}
+let currentHour;
+let tokenExpiresHour;
+
 
 router.get('/', (req, res) => {
     return res.json({ message: 'fgts router ok!' });
@@ -60,7 +72,7 @@ router.post('/calculo', async (req, res) => {
     const data = req.body;
     const headers = { headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${apiCredentials?.token}` }};
 
-    try {
+   try {
         const apiData = await axios.post(`${process.env.FACTA_BASE_URL}/fgts/calculo`, data, headers);
         console.log(apiData.data);
         return res.json(apiData.data);
@@ -71,6 +83,121 @@ router.post('/calculo', async (req, res) => {
     
 });
 
+router.post('/state/cities', async (req, res) => {
+    const { stateUF } = req.body;     
+    if(cache[stateUF]) {
+        console.log('cache data');
+        return res.json(cache[stateUF]);
+    } 
+    
+    if(!apiCredentials?.token) {
+        console.log('start get token');
+        await getToken(apiCredentials);
+        console.log('finish get token');
+
+        currentHour = new Date().getHours();
+        tokenExpiresHour = +apiCredentials?.expires?.split(':')[0];
+    }
+
+    if(apiCredentials.token && currentHour >= +tokenExpiresHour ) {
+        await getToken(apiCredentials);
+    }
+
+
+    try {
+        const apiData = await axios.get(`${process.env.FACTA_BASE_URL}/proposta-combos/cidade?estado=${stateUF}`, {
+            headers: {
+                'Authorization': `Bearer ${apiCredentials?.token}`
+            }
+        });
+
+        const sorted = Object.entries(apiData.data.cidade).sort(([, a], [, b]) => {
+            return a.nome > b.nome ? 1 : a.nome < b.nome ? - 1 : 0;
+        });
+
+        apiData.data.cidade = sorted
+        cache[stateUF] = apiData.data;
+        return res.json(apiData.data);
+    } catch (error) {
+        console.log(error);
+        return res.json({ message: 'não foi possivel concluir a operação'})
+    }
+    
+})
+
+router.post('/simulate/create', async (req, res) => {
+    const defaultParams = { produto: "D", tipo_operacao: "13", averbador: "20095", convenio: "3", login_certificado: "93231" }
+    const bodyData = { ...defaultParams, ...req.body  };
+    const formData = new FormData();
+
+    Object.entries(bodyData).forEach(([key, value]) => formData.append(key, value));
+    const headers = { headers: { 'Authorization': `Bearer ${apiCredentials?.token}` }};
+
+    try {
+        const { data } = await axios.post(`${process.env.FACTA_BASE_URL}/proposta/etapa1-simulador`, formData, headers);
+        console.log(data);
+        return res.json(data);
+    } catch (error) {
+        console.log(error);
+        return res.json({ message: 'não foi possivel concluir a operação'})
+    }
+})
+
+router.post('/user/create', async (req, res) => {
+    const data = req.body;
+    const formData = new FormData();
+    const headers = { headers: { 'Authorization': `Bearer ${apiCredentials?.token}` }};
+    Object.entries(data).forEach(([key, value]) => formData.append(key, value));
+    
+    try {
+        const { data } = await axios.post(`${process.env.FACTA_BASE_URL}/proposta/etapa2-dados-pessoais`, formData, headers);
+        console.log(data);
+        return res.json(data);
+    } catch (error) {
+        console.log(error);
+        return res.json({ message: 'não foi possivel concluir a operação'})
+    }
+})
+
+router.post('/proposal/create', async (req, res) => {
+    const { id_simulador, codigo_cliente, first_name, last_name, phone } = req.body;
+    const apiData = { id_simulador, codigo_cliente };
+    const formData = new FormData();
+    const headers = { headers: { 'Authorization': `Bearer ${apiCredentials?.token}` }};
+    Object.entries(apiData).forEach(([key, value]) => formData.append(key, value));
+
+    // const mockData = {
+    //     "erro": false,
+    //     "mensagem": "Proposta criada com sucesso",
+    //     "codigo": "53662881",
+    //     "url_formalizacao": "facta.ly/4e956e08"
+    // }
+
+    // await whatsappCreateUser({ first_name, last_name, phone });
+    // const id = await whatsappGetUserIdByPhone({ userPhone: phone });
+    // await fgtsSendFluxo({ userID: id });
+    // await fgtsSendWhatsappMessage({userID: id, contractLink: mockData.url_formalizacao });
+
+    // await sendWhatsAppLink({ codigo_af: mockData.codigo, apiCredentials });
+
+    try {
+        const { data } = await axios.post(`${process.env.FACTA_BASE_URL}/proposta/etapa3-proposta-cadastro`, formData, headers);
+        console.log(data);
+        
+        await whatsappCreateUser({ first_name, last_name, phone });
+        const id = await whatsappGetUserIdByPhone({ userPhone: phone });
+        await fgtsSendFluxo({ userID: id });
+        await fgtsSendWhatsappMessage({userID: id, contractLink: data.url_formalizacao });
+        
+        return res.json(data);
+    } catch (error) {
+        console.log(error);
+        return res.json({ message: 'não foi possivel concluir a operação'})
+    }
+
+    // return res.json(mockData)
+    
+})
 
 
 module.exports = router;
--- src/functions/fgts.js
+++ src/functions/fgts.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const FormData = require('form-data');
+
+const FGTS_HEADERS = { headers: { 'API-KEY': process.env.CONTA_LUZ_KEY } };
 
 async function getToken(apiCredentials) {
     try {
@@ -19,4 +22,90 @@ async function getToken(apiCredentials) {
     }
 }
 
-module.exports = { getToken };
+async function sendWhatsAppLink({ codigo_af= '', apiCredentials }) {
+    const bodyData = { codigo_af, tipo_envio: 'whatsapp' };
+    const formData = new FormData();
+    const headers = { headers: { 'Authorization': `Bearer ${apiCredentials?.token}` }};
+    Object.entries(bodyData).forEach(([key, value]) => formData.append(key, value));
+
+    const mockData = {
+        "erro": false,
+        "codigo_af": 53662881,
+        "mensagem": "whatsapp enviado com sucesso.",
+        "url_formalizacao": "facta.ly/4e956e08"
+    }
+
+    console.log(mockData);
+
+    return mockData;
+
+    // try {
+    //     const { data } = await axios.post(`${process.env.FACTA_BASE_URL}/proposta/envio-link`, formData, headers);
+    //     console.log(data);
+    //     return res.json(data);
+    // } catch (error) {
+    //     console.log(error);
+    //     return res.json({ message: 'não foi possivel concluir a operação'})
+    // }
+
+}
+
+
+async function whatsappCreateUser ({ phone, first_name, last_name }) {
+    const userInfo = { phone, first_name, last_name };
+
+    try {
+        await axios.post(`${process.env.CONTA_LUZ_BASE_URL}/subscriber/`, userInfo, FGTS_HEADERS);
+        return true;
+    } catch (error) {
+        console.log(error.message);        
+        return false;
+    }
+}
+
+async function whatsappGetUserIdByPhone({ userPhone = '' }) {
+    console.log(userPhone);
+    try {
+        const { data } = await axios.get(`${process.env.CONTA_LUZ_BASE_URL}/subscriber/${userPhone}`, FGTS_HEADERS); 
+        return data.id;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+
+
+async function fgtsSendFluxo({ userID = ''}) {    
+    const flowInfo = { flow: 545495 };
+    try {
+        await axios.post(`${process.env.CONTA_LUZ_BASE_URL}/subscriber/${userID}/send_flow/`, flowInfo, FGTS_HEADERS);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+
+async function fgtsSendWhatsappMessage({ userID = '', contractLink = '' }) {    
+    
+    const message = { "type": "text", "value": contractLink };
+
+    try {
+        await axios.post(`${process.env.CONTA_LUZ_BASE_URL}/subscriber/${userID}/send_message/`, message, FGTS_HEADERS);
+        return true;
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
+}
+
+
+module.exports = { 
+    getToken,
+    sendWhatsAppLink,
+    fgtsSendFluxo,
+    fgtsSendWhatsappMessage,
+    whatsappCreateUser,
+    whatsappGetUserIdByPhone
+};
